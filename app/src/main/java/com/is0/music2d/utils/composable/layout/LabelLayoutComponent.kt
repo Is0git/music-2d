eea0bce4597c86cfc47506b791e198a6b03469c8
danielpasin <danielpasin13@gmail.com>
2022-11-20 21:56:25 +0200

    Remove unused imports in LabelLayoutComponent.kt

--- app/src/main/java/com/is0/music2d/utils/composable/layout/LabelLayoutComponent.kt
+++ app/src/main/java/com/is0/music2d/utils/composable/layout/LabelLayoutComponent.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import com.is0.music2d.theme.AppTheme
 
