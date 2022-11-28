610d6afa34a6c6c14cbb1e0d59c1fca8dd7b4d6f
danielpasin <danielpasin13@gmail.com>
2022-11-28 17:38:22 +0200

    Test StorageSaveButtonComponent.kt custom semantics

---
+++ app/src/androidTest/java/com/is0/music2d/main/home/details/storage/utils/component/StorageSaveButtonComponentTest.kt
@@ -0,0 +1,40 @@
+package com.is0.music2d.main.home.details.storage.utils.component
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.performClick
+import com.is0.music2d.music.song.storage.utils.data.domain.SongStorageType
+import com.is0.music2d.theme.AppTheme
+import org.junit.Rule
+import org.junit.Test
+
+class StorageSaveButtonComponentTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun givenNotSaved_onSaveClick_showsIsSaved() {
+        composeTestRule.setContent {
+            AppTheme {
+                val isSaved = remember {
+                    mutableStateOf(false)
+                }
+
+                StorageSaveButtonComponent(
+                    isSaved = isSaved.value,
+                    onSaveClick = { isSaved.value = !isSaved.value },
+                    storageType = SongStorageType.FILESYSTEM,
+                )
+            }
+        }
+
+        val node = composeTestRule.onNode(SemanticsMatcher.expectValue(isSongSavedKey, false))
+
+        node.performClick()
+
+        composeTestRule.onNode(SemanticsMatcher.expectValue(isSongSavedKey, true)).assertExists()
+    }
+}
--- app/src/main/java/com/is0/music2d/main/home/details/storage/utils/component/StorageSaveButtonComponent.kt
+++ app/src/main/java/com/is0/music2d/main/home/details/storage/utils/component/StorageSaveButtonComponent.kt
@@ -1,14 +1,17 @@
-@file:OptIn(ExperimentalAnimationApi::class)
-
 package com.is0.music2d.main.home.details.storage.utils.component
 
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsPropertyKey
+import androidx.compose.ui.semantics.SemanticsPropertyReceiver
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import com.is0.music2d.music.song.storage.utils.composable.SaveStorageIconButtonComponent
 import com.is0.music2d.music.song.storage.utils.data.domain.SongStorageType
 import com.is0.music2d.theme.AppTheme
+
+val isSongSavedKey = SemanticsPropertyKey<Boolean>("isSongSaved")
+private var SemanticsPropertyReceiver.isSongSaved by isSongSavedKey
 
 @Composable
 fun StorageSaveButtonComponent(
@@ -18,7 +21,9 @@ fun StorageSaveButtonComponent(
     isSaved: Boolean = true,
 ) {
     SaveStorageIconButtonComponent(
-        modifier = modifier,
+        modifier = modifier.semantics {
+            isSongSaved = isSaved
+        },
         isSaved = isSaved,
         onSaveClick = onSaveClick,
         storageType = storageType,
